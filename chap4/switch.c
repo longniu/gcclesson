ac661fc1a4b94493fa9012b3823ca28c170685f2
longniu <niu.long@access-company.com>
2019-07-15 10:46:55 +0900

    completed chapter 4

---
+++ chap4/switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a, b, answer;
+	char op;
+	a = 1;
+	b = 2;
+	op = '/';
+
+	switch(op){
+		case '+':
+			answer = a + b;
+			break;
+		case '-':
+			answer = a - b;
+			break;
+		case '*':
+			answer = a * b;
+			break;
+		case '/':
+			if (b == 0) {
+				printf("divide by zero\n");
+			} else {
+				answer = a / b;
+			}
+			break;
+		default:
+			printf("operator unknown\n");
+			break;
+	}
+	printf("%d %c %d = %d\n", a, op, b, answer);
+	return 0;
+}
